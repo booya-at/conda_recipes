a15a9a8b662f5b1d8fbba47e0b091e8f3effd350
looooo <sppedflyer@gmail.com>
2017-02-04 22:48:49 +0100

    meshpy: single version external managed

--- meshpy/build.sh
+++ meshpy/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$PYTHON setup.py install
+$PYTHON setup.py install --single-version-externally-managed --record record.txt && cd test && python -m pytest
 
 # Add more build steps here, if they are necessary.
 
