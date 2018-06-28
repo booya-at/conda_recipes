17688be7870d44268a2922dde34e7d96a1daafb1
looooo <sppedflyer@gmail.com>
2018-06-28 22:49:44 +0200

    update meshpy feedstock

--- meshpy/build.sh
+++ meshpy/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+export LIBRARY_PATH=$PREFIX/lib
+
 $PYTHON configure.py
 $PYTHON setup.py install --single-version-externally-managed --record record.txt && cd test && python -m pytest
