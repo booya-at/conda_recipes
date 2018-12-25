076e45edc06f788a976a4422ddad428c035a1c6d
looooo <sppedflyer@gmail.com>
2018-12-25 23:33:12 +0100

    new meshpy (pybind11)

---
+++ py-xflr5/build.sh
@@ -0,0 +1,11 @@
+mkdir -p build
+cd build
+
+export LIBRARY_PATH=$PREFIX/lib
+
+cmake -G "Ninja" \
+      -D CMAKE_INSTALL_PREFIX=${PREFIX} \
+      -D CMAKE_BUILD_TYPE=Release \
+      ..
+
+ninja install
